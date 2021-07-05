13d9c5ca24870e70f19223bd336b184dede37055
pgmgent-eliemaes1 <eliemaes1@student.arteveledhs.be>
2021-07-05 23:05:42 +0200

    added custom logo and custom menus

---
+++ app/public/wp-content/themes/winefull/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        body: "#ECEEE9",
+        navcolor: "#3E5923",
+        textcolor: "#F3F1F1",
+      },
+      fontSize: {
+       'xxxs': '0.5rem',
+       'xxs': '0.6rem',
+      },
+       screens: {
+      '3xl': '1600px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- app/public/wp-content/themes/winefull/webpack.mix.js
+++ app/public/wp-content/themes/winefull/webpack.mix.js
@@ -1,5 +1,7 @@
 const mix= require('laravel-mix');
+require('laravel-mix-tailwind');
 
 mix.js('src/js/app.js', 'app.js')
   .sass('src/scss/app.scss', 'style.css')
+  .tailwind()
   .disableSuccessNotifications();
